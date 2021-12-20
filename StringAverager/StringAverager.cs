1fc797067d8b89efdd842d537fd66258a52cc07a
Derek <cool82413@gmail.com>
2021-12-21 01:53:59 +0800

    done

---
+++ StringAverager/StringAverager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringAverage
+{
+    public class StringAverager
+    {
+        private readonly string InvalidString = "n/a";
+
+        private static readonly Dictionary<string, int> NumberDic = new Dictionary<string, int> 
+        {
+            { "zero", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+        };
+
+        public string AverageString(string str)
+        {
+            var totalNum = 0;
+
+            var strAry = str.Trim().Split(" ");
+            foreach (var s in strAry)
+            {
+                if (!IsValid(s))
+                {
+                    return InvalidString;
+                }
+
+                totalNum += NumberDic[s];
+            }
+
+            var averageString = GetAverageString(totalNum, strAry.Count());
+
+            return averageString;
+        }
+
+        private static string GetAverageString(int totalNum, int count)
+        {
+            var average = totalNum / count;
+
+            return NumberDic.First(x => x.Value == average).Key;
+        }
+
+
+        private static bool IsValid(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            
+            return NumberDic.ContainsKey(str.ToLower());
+        }
+        
+    }
+}
---
+++ UnitTest/StringAverageTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+using NUnit.Framework;
+using StringAverage;
+
+namespace StringAverageTests
+{
+    public class Tests
+    {
+        private StringAverager _stringAverager;
+        
+        private const string InvalidString = "n/a";
+        
+        [SetUp]
+        public void Setup()
+        {
+            _stringAverager = new StringAverager();
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("Ten")]
+        public void Can_invalid_input(string input)
+        {
+           var result = _stringAverager.AverageString(input);
+           result.Should().Be(InvalidString);
+        }
+        
+        [Test]
+        [TestCase("zero nine five two", "four")]
+        [TestCase("four six two three", "three")]
+        public void Can_get_average_floored_String(string input, string expect)
+        {
+            var result = _stringAverager.AverageString(input);
+            result.Should().Be(expect);
+        }
+    }
+}
